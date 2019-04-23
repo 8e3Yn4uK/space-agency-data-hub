531d396222e3e6e3c800f5d138db9384ae500d7c
8e3Yn4uK <sashko.kondratenko@gmail.com>
2019-04-23 19:05:05 +0200

    creating repository

--- src/main/java/com/ve3yn4uk/spaceagencydatahub/dao/IMissionDAO.java
+++ src/main/java/com/ve3yn4uk/spaceagencydatahub/dao/IMissionDAO.java
@@ -1,8 +1,15 @@
 package com.ve3yn4uk.spaceagencydatahub.dao;
 
+import com.ve3yn4uk.spaceagencydatahub.entity.Mission;
+
+import java.util.List;
+
 /**
  * Created by 8e3Yn4uK on 23.04.2019
  */
 
 public interface IMissionDAO {
+
+    public List<Mission> findAll();
+
 }
--- src/main/java/com/ve3yn4uk/spaceagencydatahub/dao/MissionDAO.java
+++ src/main/java/com/ve3yn4uk/spaceagencydatahub/dao/MissionDAO.java
@@ -1,9 +1,14 @@
 package com.ve3yn4uk.spaceagencydatahub.dao;
 
+import com.ve3yn4uk.spaceagencydatahub.entity.Mission;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 /**
  * Created by 8e3Yn4uK on 23.04.2019
@@ -17,5 +22,16 @@ public class MissionDAO implements IMissionDAO {
     @Autowired
     public MissionDAO(EntityManager entityManager) {
         this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public List<Mission> findAll() {
+
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<Mission> myQuery = currentSession.createQuery("from Mission", Mission.class);
+        List<Mission> missions = myQuery.getResultList();
+
+        return missions;
     }
 }
