e097024ede9cda0beb580da212bbc2432872cd20
8e3Yn4uK <sashko.kondratenko@gmail.com>
2019-04-23 15:05:41 +0200

    Initial commit

---
+++ src/main/java/com/ve3yn4uk/spaceagencydatahub/SpaceAgencyDataHubApplication.java
@@ -0,0 +1,13 @@
+package com.ve3yn4uk.spaceagencydatahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceAgencyDataHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceAgencyDataHubApplication.class, args);
+	}
+
+}
